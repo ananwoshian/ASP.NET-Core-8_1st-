a6c98641db721f1d61d52c838204c08a9fdc84ce
ananwo <backleave@gmail.com>
2026-08-16 22:24:01 +0800

    0816 ver
    看MD學習紀錄

---
+++ 代辦事項/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using 代辦事項.Models;
+
+namespace 代辦事項.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //上面這串我沒有很理解，總之 DbContextOptions是一個語法套用在我定義的類別"AppDbContext"上，然後我這個類別裡面有一個options
+            //但我要啟用這個options的話，我就要用 base(options)才可以啟用 DbContextOptions這個語法的功能
+        }
+        public DbSet<Items> Items { get; set; }
+    }
+}
---
+++ 代辦事項/Models/Items.cs
@@ -0,0 +1,12 @@
+﻿namespace 代辦事項.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        // EFcore會自動把 Id這個變數是為主鍵,自動處理流水編號
+        public string Content {  get; set; }
+        // 不使用與 類別名稱相近的名稱
+        public bool IsDone {  get; set; }
+    }
+
+}
